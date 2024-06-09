7ce78518ba85aec8e20cdf0d28dc5ed842a7eb9f
Huan <juan.murato.cruz@gmail.com>
2024-06-08 20:54:41 -0500

    worlks?

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
-import { fileURLToPath, URL } from 'node:url'
-
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
@@ -17,7 +16,7 @@ export default defineConfig({
   plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src')
     }
   }
 })
